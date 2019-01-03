11f67ba937dba070120f92940a8837eb4a09e1a8
Julian Beck <beju1022@IWI-lkit3-10>
2019-01-03 10:41:48 +0100

    added kafka stuff

---
+++ lab4/startKafka.sh
@@ -0,0 +1,2 @@
+cd /usr/local/opt/kafka_2.11-1.0.0
+bin/kafka-server-start.sh config/server.properties
---
+++ lab4/startZooKeeper.sh
@@ -0,0 +1,2 @@
+cd /usr/local/opt/kafka_2.11-1.0.0
+bin/zookeeper-server-start.sh config/zookeeper.properties
